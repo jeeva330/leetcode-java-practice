9f2f3ddaebfb320790de78d6be65cfc2fed0d4bd
jeeva330 <jeeva.bytexl@gmail.com>
2026-08-06 20:19:12 +0530

    Create Ssolution2011.java

---
+++ Arrays/easy/leetcode 2011. Final Value of Variable After Performing Operations/Ssolution2011.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int x=0;
+        for(int i=0;i<operations.length;i++){
+            String s=operations[i];
+            if(s.equals("++X")) ++x;
+            else if(s.equals("X++")) x++;
+            else if (s.equals("--X")) --x;
+            else if(s.equals("X--")) x--;
+        }
+        return x;
+    }
+}
